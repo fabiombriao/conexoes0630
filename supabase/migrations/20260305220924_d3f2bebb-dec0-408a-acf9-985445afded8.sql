ae00fabff5d1980eec7d5e725d2f3b08bd50cc29
Fabio Morales Briao <fabiomoralesbriao@gmail.com>
2026-04-10 04:45:32 -0300

    chore: remove migrations, functions, config.toml project_id, and lovable README

--- supabase/migrations/20260305220924_d3f2bebb-dec0-408a-acf9-985445afded8.sql
+++
@@ -1,8 +0,0 @@
-CREATE POLICY "Admins can read all profiles"
-ON public.profiles
-FOR SELECT
-TO authenticated
-USING (
-  has_role(auth.uid(), 'admin'::app_role)
-  OR has_role(auth.uid(), 'group_leader'::app_role)
-);
